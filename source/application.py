9912dbde1cac6e43095320ef29dc6cf55598e5b6
UltraQbik <no1skill@yandex.ru>
2025-07-25 02:11:41 +0300

    Add arguments

--- source/application.py
+++ source/application.py
@@ -12,16 +12,35 @@ class Application:
     """
 
     def __init__(self):
-        ...
+        self.parse_input_file: str = ""
+        self.parse_output_file: str = ""
 
     def parse_cli(self):
         """
         Parses CLI
         """
 
+        # define parser
         parser = argparse.ArgumentParser(
             prog="Photon Compiler",
             description="Compiler for Scrap Mechanic CPU")
+
+        # add arguments
+        parser.add_argument(
+            "-i", "--input",
+            help="source input file",
+            required=True)
+        parser.add_argument(
+            "-o", "--output",
+            help="bytecode output file")
+
+        # parse arguments
+        args = parser.parse_args()
+
+        self.parse_input_file = args.input
+        self.parse_output_file = args.output
+        if args.output is None:
+            self.parse_output_file = self.parse_input_file + ".bin"
 
     def run(self):
         """
