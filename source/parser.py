fc216510eb079c79ae9e834114471d4e6b3ac16c
UltraQbik <no1skill@yandex.ru>
2025-07-25 02:34:38 +0300

    Define method parse

--- source/parser.py
+++ source/parser.py
@@ -3,7 +3,20 @@ Source code parser
 """
 
 
+from source.classes import *
+
+
 class Parser:
     """
     Parser class
     """
+
+    def __init__(self):
+        ...
+
+    def parse(self, code: str) -> list[TokenLine]:
+        """
+        Parses given code
+        :param code: source code
+        :return: parsed list of TokenLine's
+        """
