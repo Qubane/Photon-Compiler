437c62ceb6d875e26321ddbd6dceccfe9cd1db0e
UltraQbik <no1skill@yandex.ru>
2025-07-25 02:29:14 +0300

    Add token class

--- source/classes.py
+++ source/classes.py
@@ -3,4 +3,15 @@ Some helper classes
 """
 
 
+from typing import Any
+from dataclasses import dataclass
 
+
+@dataclass(frozen=True)
+class Token:
+    """
+    Token class
+    """
+
+    type: str
+    value: Any
