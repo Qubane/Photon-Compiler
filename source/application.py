eac23754a9248a59993124e7635e441bb554f8f6
UltraQbik <no1skill@yandex.ru>
2025-07-25 02:00:19 +0300

    Add application.py

---
+++ source/application.py
@@ -0,0 +1,17 @@
+"""
+Main application file
+"""
+
+
+class Application:
+    """
+    Application class
+    """
+
+    def __init__(self):
+        ...
+
+    def run(self):
+        """
+        Runs the application
+        """
