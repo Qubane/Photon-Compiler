9b8e6f588acba52fb608948974e443c6ec3c7d6a
UltraQbik <no1skill@yandex.ru>
2025-07-25 11:09:19 +0300

    Define attribute and compile method

--- source/compiler.py
+++ source/compiler.py
@@ -3,7 +3,20 @@ Compiles the code
 """
 
 
+from source.classes import *
+
+
 class Compiler:
     """
     Compiler class
     """
+
+    def __init__(self):
+        self._label_map: dict[str, LinkedNode] = {}
+
+    def compile(self, parsed_code: list[TokenLine]) -> list[TokenLine]:
+        """
+        Compiles parsed code
+        :param parsed_code: parsed code
+        :return: compiled code
+        """
