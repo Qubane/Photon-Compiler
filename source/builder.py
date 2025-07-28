724e2c534b4eabfb40048c8ec56e63f63ced421a
UltraQbik <no1skill@yandex.ru>
2025-07-28 20:06:00 +0300

    Change path

--- source/builder.py
+++ source/builder.py
@@ -98,31 +98,9 @@ class BlueprintBuilder:
         :param name: name for the blueprint
         """
 
-        # make uuid
-        blueprint_uuid = uuid.uuid4()
-
-        # make blueprint directory
-        directory_path = f"blueprints/{blueprint_uuid}"
-        os.mkdir(directory_path)
-
-        # make "preview" icon
-        Image.effect_noise((128, 128), 1).save(f"{directory_path}/icon.png")
-
-        # make description
-        description = {
-            "description": "bytecode generated via Photon Compiler",
-            "localId": f"{blueprint_uuid}",
-            "name": f"{name}",
-            "type": "blueprint",
-            "version": 0}
-
-        # dump description
-        with open(f"{directory_path}/description.json", "w") as file:
-            json.dump(description, file, indent=4)
-
         # dump blueprint data
         blueprint = {"bodies": [{"childs": blueprint}], "version": 4}
-        with open(f"{directory_path}/blueprint.json", "w") as file:
+        with open(f"blueprints/blueprint.json", "w") as file:
             json.dump(blueprint, file)
 
     @classmethod
