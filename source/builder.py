605b215d2303ef8c09e036df3ffed6ffdc162e42
UltraQbik <no1skill@yandex.ru>
2025-07-28 17:18:31 +0300

    Add "blueprints" directory

--- source/builder.py
+++ source/builder.py
@@ -3,4 +3,9 @@ Builds compiled code to bytecode and to scrap mechanic blueprints
 """
 
 
+import os
 
+
+# make blueprints directory
+if not os.path.isdir("blueprints"):
+    os.mkdir("blueprints")
