cc4e36936d8d04dc52a593e3d8538c84169c4271
UltraQbik <no1skill@yandex.ru>
2025-07-28 12:16:59 +0300

    Get rid of linked lists

--- source/classes.py
+++ source/classes.py
@@ -3,11 +3,6 @@ Some helper classes
 """
 
 
-from enum import Enum
-from typing import Any, Union, Iterable
-from dataclasses import dataclass
-
-
 class TokenLine(list):
     """
     Container for multiple tokens
@@ -17,46 +12,3 @@ class TokenLine(list):
         super().__init__(*args)
 
         self.reference_line: int = reference_line
-
-
-@dataclass
-class LinkedNode:
-    """
-    Node for linked list
-    """
-
-    value: Any
-
-    next_node: Union["LinkedNode", None] = None
-
-
-class LinkedList:
-    """
-    Linked list class
-    """
-
-    def __init__(self, iterable: list):
-        self.start_node: LinkedNode = LinkedNode(iterable[0])
-        self._iter_node: LinkedNode = self.start_node
-
-        current_node = self.start_node
-        for item in iterable[1:]:
-            # define next node
-            node = LinkedNode(item)
-
-            # assign next node to previous node
-            current_node.next_node = node
-
-            # reassign next node to be the current one
-            current_node = node
-
-    def __iter__(self):
-        self._iter_node = self.start_node
-        return self
-
-    def __next__(self):
-        if self._iter_node is None:
-            raise StopIteration
-        value = self._iter_node
-        self._iter_node = self._iter_node.next_node
-        return value
